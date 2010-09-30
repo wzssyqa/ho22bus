105d416a673431a1a108addc074608ec35e2c1ae
YunQiang Su <wzssyqa@gmail.com>
2010-09-30 13:20:06 +0800

    TTS fixed

--- src/readword.cpp
+++ src/readword.cpp
@@ -6,42 +6,34 @@ extern CReciteWord* g_pReciteWord;
 
 
 Cwyabdc::Cwyabdc ()
-{	
-	gchar *filename=g_build_filename(reciteword_data_dir,"TTS",NULL);
-	havedatafile = g_file_test(filename, G_FILE_TEST_EXISTS);
-	if (!havedatafile)
-		g_print("RealPeopleTTS files not found!\n");
-	g_free(filename);
+{
 }
 
 Cwyabdc::~Cwyabdc ()
 {
-
 }
 
 void Cwyabdc::read(const char *word,PLAY_METHOD method)
 {
-	if (havedatafile) {
-		int n=strlen(word);
-		gchar *lowerword = (gchar *)g_malloc(n+1);
-		for (int i=0;i<n;i++)
-			lowerword[i]= g_ascii_tolower(word[i]);
-		lowerword[n] = '\0';
+	int n=strlen(word);
+	gchar *lowerword = (gchar *)g_malloc(n+1);
+	for (int i=0;i<n;i++)
+		lowerword[i]= g_ascii_tolower(word[i]);
+	lowerword[n] = '\0';
 		
-		gchar *tmpfn = g_strdup_printf("%c/%s.wav", lowerword[0],lowerword);
-		gchar *filename=g_build_filename(reciteword_data_dir,"TTS",tmpfn,NULL);
-		g_free(tmpfn);	
+	gchar *tmpfn = g_strdup_printf("%c/%s.wav", lowerword[0],lowerword);
+	gchar *filename=g_build_filename(reciteword_data_dir,"TTS",tmpfn,NULL);
+	g_free(tmpfn);	
 	
-		if (g_file_test(filename, G_FILE_TEST_EXISTS))
-			play_file (filename,method);
-		else{
-			gchar *cmd = g_strdup_printf("espeak %s",lowerword);
-			system(cmd);
-			g_free(cmd);
-		}
-		g_free(filename);
-		g_free(lowerword);
+	if (g_file_test(filename, G_FILE_TEST_EXISTS))
+		play_file (filename,method);
+	else{
+		gchar *cmd = g_strdup_printf("espeak %s",lowerword);
+		system(cmd);
+		g_free(cmd);
 	}
+	g_free(filename);
+	g_free(lowerword);
 }
 
 // if use global static class,the class's constructor will run before main(),it means that the constructor run before gtk_init(),
